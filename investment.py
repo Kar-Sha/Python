644533b840b5345a208c2e1adee43b17702d4d93
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2021-03-05 20:56:02 -0800

    Update investment.py

--- investment.py
+++ investment.py
@@ -1,24 +1,19 @@
-def calculate_apr():
+def calculate_apr(principal, interest_rate, years):
   '''
   This function is used to calculate how much money can be made in an investment over time
-  using the formula principal*(1+(interest_rate))
+  with the principal, interest_rate, and years parameters that are then 
+  being used in the formula principal*(1+(interest_rate))
   '''
-#both principal and interest_rate parameters are floats while years is in int
-  principal = float(input())
-  interest_rate = (float(input()) / 100)
-  years = int(input())
-  total = 0
-  for i in range(years):
-    total = total + (principal * (1 + (interest_rate))
-                     
- #if any parameters are negative, return false
+  #if any parameters are negative, return false
   if principal < 0:
     return False
-  elif interest_rate < 0:
+  if interest_rate < 0:
     return False
-  elif years < 0:
+  if years < 0:
     return False
-  else:
-    print(total)
-
+  total = 0
+  #loop by how many times the years value is
+  for i in range(years):
+    total = total + (principal * (1 + (interest_rate))
+  return float(total)
 calculate_apr()
